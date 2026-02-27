b2c47c7ad27323e01221c088db3fd1071c97640f
Kieran Kelly <kieranjokelly@gmail.com>
2026-02-27 09:48:05 -0500

    Add initial implementation of office hours triage system

---
+++ officehours.cpp
@@ -0,0 +1,15 @@
+//office hours triage system
+//first layer: arrival queue
+//second layer: skill set queue
+//third layer: TA queue (length 1)
+
+// student arrives, goes to arrival queue
+// student is sorted into skill set queue based on their question
+// student is sorted from the front of the skill set queue to the TA queue when the TA is available
+
+#include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
